af71e00fc3cc8aacc1ed02672495ad4f9144b5d2
mrfakename <me@mrfake.name>
2022-07-12 18:47:49 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,158 @@
-# PHP-GIF-Reborn
+| :warning: This repository was forked from @ErikvdVen's [php-gif](https://github.com/ErikvdVen/php-gif). It is minorly updated to work with PHP 8.
+| ---
+# PHP GIF Reborn
+[![Latest Stable Version](https://poser.pugx.org/erikvdven/php-gif/v/stable)](https://packagist.org/packages/erikvdven/php-gif)
+[![Donate](https://img.shields.io/badge/Donate-PayPal-green.svg)](https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=erikvandeven100%40hotmail%2ecom&lc=NL&item_name=Erik%20van%20de%20Ven%20Github)
+
+Below GIF image is an example image which can contain real-time data. A PHP script calculates the pending time till new year and generates the GIF image. An ideal solution for sending e-mails with real-time data to customers. E-mail clients give you the opportunity to load images by URL and so everytime the client re-opens the e-mail message, the GIF will be re-generated with real-time data.
+
+For below image this doesn't work, unfortunately, because GitHub downloads the image once and stores it locally. Most e-mail clients, however, do load the images directly from source. Successfully tested with Gmail and Outlook 2011 (Mac OSX). 
+
+*Important note: Outlook 2007, 2010 and 2013 for Windows will only show the first frame. TIP!: Keep the first frame universal, so it doesn't matter the GIF image animates or not.*
+
+_There used to be a demo of the countdown below, but it is currently not working as of March 2022._
+![Live countdown to new year](http://only-media.nl/gif/gif.php)
+
+## Important Information
+This package relies on the Lato font. You can download it from Google Fonts [here](https://fonts.google.com/specimen/Lato). Copy the TTF files into the fonts folder. Otherwise, the GIF will be blank.
+
+## Installing PHP GIF Reborn
+1. Download the repo
+2. Open the src/Gif folder
+3. Copy GIFEncoder.php and GIFGenerator.php into your file
+4. Paste the following code at the top of your file:
+```php
+include 'GIFEncoder.php';
+include 'GIFGenerator.php';
+```
+## Getting Started
+
+Create a PHP file and add these headers at the beginning of the file:
+
+```php
+// Caching disable headers
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
+// Output as a GIF image
+header ('Content-type:image/gif');
+
+// Import the GIFEncoder and GIFGenerator PHP files
+include 'GIFEncoder.php';
+include 'GIFGenerator.php';
+```
+
+On the next lines you can create a GIF image by first initializing the GIFGenerator object and creating an array with all the image frames:
+
+```php
+// Initialize a new GIFGenerator object
+$gif = new GIFGenerator();
+
+// Create a multidimensional array with all the image frames
+$imageFrames = array(
+	'repeat' => false,
+	'frames' => array(
+		array(
+			'image' => './images/newyear.jpg',
+			'text' => array(
+				array(
+					'text' => 'Hello GIF frame 1',
+					'fonts-color' => '#000',
+					'x-position' => 140,
+					'y-position' => 138
+				)
+			),
+			'delay' => 100
+		),
+	)
+);
+```
+Finally you generate the image and `echo` the results on the screen: 
+
+```php
+echo $gif->generate($imageFrames);
+```
+
+## Example
+
+A more complete example. You could copy/paste below code to a file and execute it in the browser to view a more complete result. As you can see it's not required to use text in your GIF image and you can add as much text per frame, and as much frames per GIF image as you like.
+
+```php
+<?php
+// Caching disable headers
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
+// Output as a GIF image
+header ('Content-type:image/gif');
+
+// Include the GIFEncoder and GIFGenerator PHP files
+include 'GIFEncoder.php';
+include 'GIFGenerator.php';
+
+// Initialize a new GIFGenerator object
+$gif = new GIFGenerator();
+
+// Create a multidimensional array with all the image frames
+$imageFrames = array(
+	'repeat' => 5,
+	'frames' => array(
+		array(
+			'image' => './images/newyear.jpg',
+			'text' => array(
+				array(
+					'text' => 'Hello GIF frame 1',
+					'fonts' => './fonts/Lato-Light.ttf',
+					'fonts-size' => 30,
+					'angle' => 0,
+					'fonts-color' => '#000',
+					'x-position' => 140,
+					'y-position' => 138
+				)
+			),
+			'delay' => 100
+		),
+		array(
+			'image' => './images/newyear.jpg',
+			'text' => array(
+				array(
+					'text' => 'Hello GIF frame 2',
+					'fonts' => './fonts/Lato-Light.ttf',
+					'fonts-size' => 15,
+					'angle' => 0,
+					'fonts-color' => '#000',
+					'x-position' => 140,
+					'y-position' => 138
+				),
+				array(
+					'text' => 'Hello GIF frame 2',
+					'fonts' => './fonts/Lato-Light.ttf',
+					'fonts-size' => 15,
+					'angle' => 0,
+					'fonts-color' => '#000',
+					'x-position' => 140,
+					'y-position' => 108
+				)
+			),
+			'delay' => 100
+		),
+		array(
+			'image' => './images/newyear.jpg',
+			'delay' => 50
+		)
+	)
+);
+
+echo $gif->generate($imageFrames);
+?>
+```
+
+## License & Credits
+
+This software is published under the [MIT License](https://en.wikipedia.org/wiki/MIT_License).
+
+###### GIFEncoder
+
+GIFEncoder.class.php contains minor adaptations from the GIFEncoder PHP class by [László Zsidi](http://gifs.hu).
---
+++ src/Gif/GIFGenerator.php
@@ -0,0 +1,178 @@
+<?php
+Class GIFGenerator {
+
+    private $_defaultYPosition;
+    private $_defaultXPosition;
+    private $_defaultAngle;
+    private $_defaultFont;
+    private $_defaultFontColor;
+    private $_defaultFontSize;
+    private $_defaultDelay;
+    private $_defaultRepeat;
+
+    /**
+     * Constructor of the GIFGenerator object which sets the default values
+     *
+     * @param array $kwargs default values to override
+     */
+    function __construct(array $kwargs = []) {
+
+        // Set defaults
+        $defaults = [
+            "y-position" => 100,
+            "x-position" => 100,
+            "angle" => 0,
+            "fonts" => __DIR__.'/fonts/RobotoMono-Bold.ttf',
+            "fonts-color" => [255,255,255],
+            "fonts-size" => 12,
+            "delay" => 100,
+            "repeat" => 0
+        ];
+
+        // Overwrite all the defaults with the arguments
+        $args = array_merge($defaults,$kwargs);
+
+        $this->_defaultYPosition = $args['y-position'];
+        $this->_defaultXPosition = $args['x-position'];
+        $this->_defaultAngle = $args['angle'];
+        $this->_defaultFont = $args['fonts'];
+        $this->_defaultFontColor = $args['fonts-color'];
+        $this->_defaultFontSize = $args['fonts-size'];
+        $this->_defaultDelay = $args['delay'];
+        $this->_defaultRepeat = $args['repeat'];
+    }
+
+    /**
+     * imagettftext with letter-spacing as extra feature
+     *
+     * @param  string  $image   	background image of the GIF
+     * @param  integer $fontsize    fontsize of the text
+     * @param  integer $angle   	rotation angle of the text
+     * @param  integer $x       	x-position of the text inside the image
+     * @param  integer $y       	y-position of the text inside the image
+     * @param  string  $color   	text color
+     * @param  integer $font    	font-family fo the text
+     * @param  string  $text    	the actual text
+     * @param  integer $spacing 	letter-spacing of the text
+     * @return void
+     */
+    private function imagettftextSp($image, $fontsize, $angle, $x, $y, $color, $font, $text, $spacing = 0) {
+        if ($spacing == 0) {
+            $txt = imagettftext($image, $fontsize, $angle, $x, $y, $color, $font, $text);
+        } else {
+            $temp_x = $x;
+            for ($i = 0; $i < strlen($text); $i++) {
+                $txt = imagettftext($image, $fontsize, $angle, $temp_x, $y, $color, $font, $text[$i]);
+                $temp_x += $spacing + ($txt[2] - $txt[0]);
+            }
+        }
+    }
+
+    /**
+     * Generates the actual GIF image
+     *
+     * @param  array  	$array array with all image frames
+     * @return resource        returns the actual GIF image
+     */
+    public function generate(array $array) {
+        $frames = [];
+        $frame_delay = [];
+
+        foreach($array['frames'] as $frame) {
+
+            $image = $this->_createImage($frame['image']);
+
+            if(array_key_exists('text', $frame))
+                foreach($frame['text'] as $key => $text) {
+
+                    // Set defaults
+                    $defaults = [
+                        "angle" => $this->_defaultAngle,
+                        "fonts" => $this->_defaultFont,
+                        "fonts-color" => $this->_defaultFontColor,
+                        "fonts-size" => $this->_defaultFontSize,
+                        "y-position" => $this->_defaultYPosition,
+                        "x-position" => $this->_defaultXPosition,
+                        "text" => null,
+                        "letter-spacing" => 0
+                    ];
+
+                    // Overwrite all the defaults with the arguments
+                    $args = array_merge($defaults, $text);
+                    $fontColor = is_array($args['fonts-color']) ? $args['fonts-color'] : $this->_hex2rgb($args['fonts-color']);
+                    $text_color = imagecolorallocate($image, $fontColor[0], $fontColor[1], $fontColor[2]);
+
+                    $this->imagettftextSp(
+                        $image,
+                        $args['fonts-size'],
+                        $args['angle'],
+                        $args['x-position'],
+                        $args['y-position'],
+                        $text_color,
+                        $args['fonts'],
+                        $args['text'],
+                        $args['letter-spacing']);
+                }
+
+            $delay = (array_key_exists('delay', $frame)) ? $frame['delay'] : $this->_defaultDelay;
+
+            ob_start();
+            imagegif($image);
+            $frames[]=ob_get_contents();
+            $frame_delay[]=$delay; // Delay in the animation.
+            ob_end_clean();
+        }
+
+
+        $repeat = (array_key_exists('repeat', $array)) ? $array['repeat'] : $this->_defaultRepeat;
+        $gif = new GIFEncoder($frames,$frame_delay,$repeat,2,0,0,0,0,'bin');
+        return $gif->GetAnimation();
+    }
+
+    /**
+     * Creates an actual GIF image from the given source
+     *
+     * @param  string 	$imagePath path to the image
+     * @return resource            returns the image
+     */
+    private function _createImage($imagePath) {
+        $cImage = null;
+        $tmp = explode('.', $imagePath);
+        $ext = end($tmp);
+
+        switch(strtolower($ext)){
+            case 'jpg':
+            case 'jpeg':
+                $cImage = imagecreatefromjpeg($imagePath);
+                break;
+            case 'png':
+                $cImage = imagecreatefrompng($imagePath);
+                break;
+        }
+
+        return $cImage;
+    }
+
+    /**
+     * Converts hexadecimal color string to an array with rgb values
+     *
+     * @param  string $hex the hexadecimal color which needs to be converted
+     * @return array       returns an array with the rgb values
+     */
+    private function _hex2rgb($hex) {
+        $hex = str_replace("#", "", $hex);
+
+        if(strlen($hex) == 3) {
+            $r = hexdec(substr($hex,0,1).substr($hex,0,1));
+            $g = hexdec(substr($hex,1,1).substr($hex,1,1));
+            $b = hexdec(substr($hex,2,1).substr($hex,2,1));
+        } else {
+            $r = hexdec(substr($hex,0,2));
+            $g = hexdec(substr($hex,2,2));
+            $b = hexdec(substr($hex,4,2));
+        }
+        $rgb = [$r, $g, $b];
+
+        return $rgb;
+    }
+}
